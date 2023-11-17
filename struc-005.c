e63857471b9284e51651f918fad8cb381d1e7d41
samwanyua <samexample8@gmail.com>
2023-11-17 12:59:14 +0300

    Use of Macros

---
+++ struc-005.c
@@ -0,0 +1,44 @@
+/*Use of Macro*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_ELEPHANT_SEAL_WT_MALE 8800
+#define MIN_ELEPHANT_SEAL_WT_MALE 4400
+
+//Note female range is 880 - 2200 lbs
+
+//produce a macro that randomlt generate a male weight
+
+#define RANGE 4400 //Gap between max and min
+#define POPULATION 1000
+#define WEIGHT_OVER  rand()%RANGE 
+#define WEIGHT   WEIGHT_OVER + MIN_ELEPHANT_SEAL_WT_MALE
+#define FILL  for(i =0; i < POPULATION; i++) \
+                    data[i] = WEIGHT
+
+void print_data(int d[],int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d\t",d[i]);
+        if ((i + 1) % 10 == 0)
+        {
+            printf("\n");
+        }
+
+}
+
+int main(void)
+{
+
+    int i;
+    int data[POPULATION];
+    srand(time(0));
+    FILL;
+    print_data(data, POPULATION);
+    printf("\n\n");
+
+    return 0;
+}
