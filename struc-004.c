e9dea2797453fced70b00e18d270a1f3813dd2aa
samwanyua <samexample8@gmail.com>
2023-11-17 12:40:42 +0300

    Preprocessor

---
+++ struc-004.c
@@ -0,0 +1,38 @@
+/*C preprocessor*/
+
+#include <stdio.h>
+
+int main(void)
+{
+/*
+The preprocessor **chapter 8 >> A book on C
+
+#include <stdio.h> // prontf and scanf. standard header file
+#include <math.h> //math functions
+
+#include "filename"
+
+#define  is a basic macro facility: it helps to extend the language
+
+#define C = speed of light
+#define EQ == 
+# define SQ(x) ((x) * (x))
+
+
+    
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+    return 0;
+}
