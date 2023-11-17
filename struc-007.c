8bb4ebf68b61276173e0d0d5811386a3f31b2f25
samwanyua <samexample8@gmail.com>
2023-11-17 13:20:15 +0300

    Simple assert code

---
+++ struc-007.c
@@ -0,0 +1,15 @@
+/*Assert code*/
+#define NDEBUG
+#include <assert.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    assert(0);
+    printf("My program runs.\n");
+    
+
+
+    return 0;
+}
