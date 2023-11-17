2d4c66f8f76fb55d5d3c88e7dcf87856b694152d
samwanyua <samexample8@gmail.com>
2023-11-17 13:11:41 +0300

    Use of asserts for program correctness

---
+++ struc-006.c
@@ -0,0 +1,42 @@
+/*Use of assert for programs correctedness*/
+
+#include <stdio.h>
+
+int main(void)
+{
+/*
+
+Macros id stdio.h, ctype.h
+
+#define getchar() getc(stdin)
+// helps in getting characters from input
+
+#define putchar(c) putc(ic), stdout
+//places characters in output
+
+ctype.h
+//is-bool
+isalpha(c) ... alphabet
+isspace(c) ...whitespace
+isdigit(c) ...number
+islower(c)  ...lowercase
+
+#include <assert.h>
+NDEBUG  >> turns asserts on/off
+
+asserts are used to prove the correctness of your program
+They can make the program clearer
+
+
+
+
+
+
+
+
+
+
+
+*/
+    return 0;
+}
