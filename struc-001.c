b4fd426707684f811d7be8bc72594697a0a7c044
samwanyua <samexample8@gmail.com>
2023-11-17 11:59:51 +0300

    Enum as ADT

---
+++ struc-001.c
@@ -0,0 +1,33 @@
+/*Enums as an ADT*/
+
+#include <stdio.h>
+
+int main(void)
+{
+/*
+* ADT is User Defined Types
+* Enum is Enumaration
+*useful for different data but not all data
+eg. Days of the week
+
+*Type safe languages make debugging easier.
+
+example
+    enum day { sun, mon, tue, wed, thur, fri, sat};
+    enum day today = fri;
+
+    sun == 0 this is a default initialization of enumarators
+    mon == 1 this is a default initialization of enumarators
+
+    enum is a form of int type
+
+
+
+
+
+*/
+
+
+    return 0;
+}
+
