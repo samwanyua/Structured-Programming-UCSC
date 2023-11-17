5b9ea80594797691e3f647d9028b94517337aed6
samwanyua <samexample8@gmail.com>
2023-11-17 13:49:58 +0300

    Introduction to strucs

---
+++ struc-009.c
@@ -0,0 +1,40 @@
+/*Introduction to struc*/
+#include <stdio.h>
+
+int main(void)
+{
+
+/*
+Structures   **Chapter 9
+Abstract data type
+>>Types
+- Critical to a problem solution
+- native types: int , char, floats
+
+struc cand{
+    int pips;
+    char suit;
+    } 
+deck[52];
+
+typedef // to simplify declarations
+
+    typedef struct 
+    { 
+        float re;
+        float im;
+    } complex;
+    complex a,b,c[10];
+
+
+
+
+
+
+
+
+*/
+
+
+    return 0;
+}
