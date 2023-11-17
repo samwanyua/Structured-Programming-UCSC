4708877168ef1599f8bcb9e8d386f969d6855a32
samwanyua <samexample8@gmail.com>
2023-11-17 16:01:46 +0300

    Abstract Data Type Stack intro

---
+++ struc-011.c
@@ -0,0 +1,30 @@
+/*Intro to ADT Stack*/
+#include <stdio.h>
+int main(void)
+{
+/*
+The Abstract Data Type stack (ADT)
+> canonical non-trivial data structure: appears in both computer algorithms and in computer architecture
+
+Operations: push, pop, top, empty, full, reset
+
+typedef struct stack{
+    char s [MAX_LEN];
+    int top;
+} stack;
+
+//underlying rep is an array
+
+push
+
+    void push (char c, stack * stk)
+    {
+       stk -> top++;
+       stk -> s[stk->top] = c;
+    }
+
+
+*/
+
+    return 0;
+}
