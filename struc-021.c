3c126052f9653d4f4360631c02ce35bc88ce4720
samwanyua <samexample8@gmail.com>
2023-11-18 11:17:00 +0300

    Advanced I/0 Intro

---
+++ struc-021.c
@@ -0,0 +1,40 @@
+/*Advanced I/O printf*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*
+printf goes to the screen and uses stdout
+scanf goes to the keyboard and uses stdin
+
+redirection where code.exe > file
+
+print is redirected to file code.exe < file
+
+scanf input comes from file
+
+printf 
+>> has a control string and other arguements
+eg. printf("sum of x and y is %d and %d = %d", x,y, x+y);
+%d is conversion to a decimal integer
+
+
+#conversion table
+#format specifiers modifiers >> modifiers and control codes
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+    return 0;
+}
