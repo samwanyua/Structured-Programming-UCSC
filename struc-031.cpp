dbd2c5f5ad65da4ab6175cabf514350231740ece
samwanyua <samexample8@gmail.com>
2023-11-18 19:20:39 +0300

    C++ features that are easy

---
+++ struc-031.cpp
@@ -0,0 +1,50 @@
+/*C++ features that are easy
+
+<iostream> 
+cout << "message";
+cin >> x;
+
+inline - for optimization
+
+for expressions allows local declarations
+
+    for( int i = 0; i < N; ++i)
+    {
+        ...
+    }
+
+    you can put declarations anywhere
+    : code
+    double area;
+    area = h *w;
+    cout << "Area is : ";
+
+    inferred typing with
+    * auto
+
+    auto i = 3;
+    auto x = 3.0; //duck typing
+    // very useful when types get very complicated.
+    // for simple types keep normal type declarations
+
+    STL = Standard Template Library
+* It add many containers (lists, stacks occur here)
+* very efficient algorithms for use
+
+vector is a powerful and flexible kind of an array, can be indexed
+
+vector <int> a (N);
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
