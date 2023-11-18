12919ff1db9053a2a6ff7b4211d8c8a5fba91cf3
samwanyua <samexample8@gmail.com>
2023-11-18 11:25:51 +0300

    Test modifiers

---
+++ struc-022.c
@@ -0,0 +1,10 @@
+/*Test modifiers*/
+#include <stdio.h>
+
+int main(void)
+{
+    double x = 0.123456789;
+    printf("general Printing ideas\n\n");
+    printf("x is %-12.5e and %e and %10.5f and %10d\n\n", x,x,x,x);
+    return 0;
+}
