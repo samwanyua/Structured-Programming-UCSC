330537f27f17b4620c01be8ea30994b3ed7c8a45
samwanyua <samexample8@gmail.com>
2023-11-18 19:42:25 +0300

    C++ ADT as a Class

---
+++ struc-033.cpp
@@ -0,0 +1,30 @@
+/*C++ ADT as a class*/
+
+#include <iostream>
+
+/*
+ADT = Class
+Encapsulation allows you to hide stuff!
+
+class rectangle {
+    public:
+        rectangle( double h, double w);
+        double area { return height * width;}
+
+    private: //hide away the representation
+        double height, width;
+    
+}
+
+    rectangle r (2.5,2.0);
+    double ar;
+    ar = r.area();
+
+
+
+
+
+
+
+
+*/
