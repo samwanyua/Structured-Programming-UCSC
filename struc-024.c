8daad96d12858e394d1616355cad75f6c8c874b6
samwanyua <samexample8@gmail.com>
2023-11-18 14:50:41 +0300

    File I/O intro

---
+++ struc-024.c
@@ -0,0 +1,23 @@
+/*Intro to File I/O*/
+
+#include <stdio.h>
+int main(void)
+{
+/*
+Chapter 11
+fscanf(FILE->fp, ...) //pointer to  a file
+so it is like scanf but needs  a named file
+
+Opening a file for reading
+    ifp = fopen ("filename", "r"); //r = reading the file
+    //ifp is input file pointer declared as 
+        FILE *ifp
+
+    To close;
+        f(close(ifp));
+    
+
+*/
+
+    return 0;
+}
