6533c3dcaa6a9f85ce752d5736e6a5df4be32b61
samwanyua <samexample8@gmail.com>
2023-11-18 10:39:17 +0300

    Intro to Binary tree

---
+++ struc-019.c
@@ -0,0 +1,25 @@
+/*Intro to Binary Trees*/
+#include <stdio.h>
+
+int main()
+{
+/*
+Binary Trees **chapter 10
+
+nodes -> a value is stored there
+Root
+Branch
+Leaf
+
+log2(n) highl
+
+A complete binary tree of depth 3  would have  8 leaf nodes
+
+
+
+
+
+*/
+
+    return 0;
+}
