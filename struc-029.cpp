e1951bd28273001655430173ee63b30906bd9a67
samwanyua <samexample8@gmail.com>
2023-11-18 18:49:41 +0300

    Intro to C++

---
+++ struc-029.cpp
@@ -0,0 +1,34 @@
+/*
+Chapter 13 #A Book of c
+
+C is essentially a subset of C++
+C++ has many improvements that makes programming easier
+C++ compilers are frequently bundled with c compilers
+ so g++ is available
+
+ easier basic I/0
+
+    cout << "C++ is an improved C";
+
+    << is overloaded to provide output behavior
+  also input
+   cin >> miles; //no need for formats or &
+
+   History
+Invented in Bell Labs in 1985 by Bjarne Stroustrup
+Originally called C with classes
+
+Adv:
+* Free
+* Based on C
+* Efficient
+
+C++ 11 version
+
+most C++ compilers will compile C programs.  
+
+
+
+
+
+*/
