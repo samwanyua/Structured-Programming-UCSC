d0f466f853de01a165754d92a00505997b201491
samwanyua <samexample8@gmail.com>
2023-11-18 10:10:39 +0300

    Full list code

---
+++ struc-017.c
@@ -0,0 +1,56 @@
+/*Full list code*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+typedef struct list { int data; struct list *next;} list;
+
+int is_empty(const list *l) {return (l == NULL);}
+list* create_list(int d)
+{
+    list* head = malloc(sizeof(list));
+    head -> data = d;
+    head -> next = NULL; //null pointer
+    return head;
+}
+
+list* add_to_front(int d, list* h)
+{
+    list* head = create_list (d);
+    head -> next = h;
+    return head;
+
+}
+
+list array_to_list(int d[], int size) //converting array to list
+{
+    list* head = create_list(d[0]);
+    int i;
+    for(i = 1; i < size; i++)
+    {
+        head = add_to_front(d[i], head);
+    }
+    
+    return head;
+}
+
+void print_list(list* h, char *title)
+{
+    printf("%s\n", title);
+    while (h != NULL)
+    {
+        printf("%d :", h -> data);
+        h = h -> next;
+    }
+}
+
+int main()
+{
+    list list_of_int;
+    list* head = NULL;
+    int data[6] = {2,3,5,7,8,9};
+    print_list(head, "data[6]  made into a 6 element list");
+    printf("\n\n");
+    return 0;
+}
