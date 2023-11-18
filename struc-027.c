251769d37b13ac73881febbff36a9c1366085d02
samwanyua <samexample8@gmail.com>
2023-11-18 15:51:53 +0300

    Use of Main (argc, argv) intro

---
+++ struc-027.c
@@ -0,0 +1,22 @@
+/*Use of Main (argc, argv)*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+/* **chapter 11
+Command line arguments
+
+inputfile - process - outputfile
+
+int main(int argc, char *argv[]);
+//argc = arguement of count
+
+so ifp = fopen(argv[1], "r");
+   ofp = fopen(argv[2], "w");
+
+
+*/
+    return 0;
+}
